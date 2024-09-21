decb3702e79cf810b55750bfa658af18bf291529
Yann Toussaint <yann.toussaint2@etu.unistra.fr>
2024-09-21 19:01:25 +0200

    tuto 2_3

---
+++ TP_Prog_Graphique_3D/tuto2_3.cpp
@@ -0,0 +1,122 @@
+/*******************************************************************************
+* EasyCppOGL:   Copyright (C) 2019,                                            *
+* Sylvain Thery, IGG Group, ICube, University of Strasbourg, France            *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Contact information: thery@unistra.fr                                        *
+*******************************************************************************/
+
+#include <iostream>
+#include "shader_program.h"
+#include "gl_viewer.h"
+#include "mesh.h"
+
+#define macro_str(s) #s
+#define macro_xstr(s) macro_str(s)
+#define DATA_PATH std::string(macro_xstr(DEF_DATA_PATH))
+#define SHADERS_PATH std::string(macro_xstr(DEF_SHADERS_PATH))
+
+// using namespace EZCOGL;
+
+class Viewer: public EZCOGL::GLViewer
+{
+	EZCOGL::ShaderProgram::UP shaderPrg;
+
+	int nbVertices;
+    int nbInstances;
+
+public:
+	Viewer();
+	void init_ogl() override;
+	void draw_ogl() override;
+	void interface_ogl() override;
+};
+
+int main(int, char**)
+{
+	Viewer v;
+	return v.launch3d();
+}
+
+Viewer::Viewer() : nbVertices(40), nbInstances(10)
+{}
+
+void Viewer::init_ogl()
+{
+	shaderPrg = EZCOGL::ShaderProgram::create({{GL_VERTEX_SHADER, EZCOGL::load_src(SHADERS_PATH + "/TUTO2_3.vs")}, {GL_FRAGMENT_SHADER, EZCOGL::load_src(SHADERS_PATH + "/TUTO2_3.fs")}}, "ImplicitGeom");
+
+	EZCOGL::VAO::none()->bind();
+
+	// ***********************************
+	// Geometry
+	// ***********************************
+    
+	// NO GEOMETRY : WE WILL CREATE IT IN THE VERTEX SHADER !
+
+	// set scene center and radius for the init of matrix view/proj
+	set_scene_center(EZCOGL::GLVec3(0.f, 0.f, 0.f));
+	set_scene_radius(2.f);
+
+	// Define the color to use when refreshing screen
+    glClearColor(0.1, 0.1, 0.1, 1.0);
+}
+
+void Viewer::draw_ogl()
+{
+	// Clear the buffer before to draw the next frame
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_PROGRAM_POINT_SIZE);
+
+    // Get the view and projection matrix
+	const EZCOGL::GLMat4& view = this->get_view_matrix();
+	const EZCOGL::GLMat4& proj = this->get_projection_matrix();
+
+    // Construct a model matrix
+    const EZCOGL::GLMat4& model = EZCOGL::Transfo::scale(1.f);
+
+	// ***********************************
+	// Rendering
+	// ***********************************
+	glDrawArraysInstanced(GL_LINE_STRIP, 0/*first index of primitive*/, nbVertices, nbInstances);
+	
+    // activate Z-buffer
+	glEnable(GL_DEPTH_TEST);
+
+    shaderPrg->bind();
+
+	// Uniforms variables send to the GPU
+	EZCOGL::set_uniform_value(0, model);
+	EZCOGL::set_uniform_value(1, view);
+	EZCOGL::set_uniform_value(2, proj);
+	EZCOGL::set_uniform_value(3, nbVertices);
+	EZCOGL::set_uniform_value(4, nbInstances);
+}
+
+void Viewer::interface_ogl()
+{
+	ImGui::GetIO().FontGlobalScale = 3.0f;
+	ImGui::Begin("Tuto 2", nullptr, ImGuiWindowFlags_NoSavedSettings);
+	ImGui::SetWindowSize({0,0});
+
+	ImGui::Text("FPS :(%2.2lf)", fps_);
+	if (ImGui::Button("Reload shaders"))
+		shaderPrg = EZCOGL::ShaderProgram::create({{GL_VERTEX_SHADER, EZCOGL::load_src(SHADERS_PATH + "/TUTO2_3.vs")}, {GL_FRAGMENT_SHADER, EZCOGL::load_src(SHADERS_PATH + "/TUTO2_3.fs")}}, "ImplicitGeom");
+
+	ImGui::SliderInt("#Vertices", &nbVertices, 2, 40);
+	ImGui::SliderInt("#Instances", &nbInstances, 2, 40);
+
+	ImGui::End();
+}
